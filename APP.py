f6519171c2e23e6a324e5c62f59e6c8cbdc83a30
chaitanyapappala983 <chaitanyapappala983@gmail.com>
2025-12-24 10:20:35 +0530

    Add files via upload

---
+++ APP.py
@@ -0,0 +1,48 @@
+import json
+import random
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfVectorizer
+import streamlit as st
+intents = json.load(open('c:\\Users\\chait\\Downloads\\intents.json'))
+tags=[]
+patterns=[]
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        patterns.append(pattern)
+        tags.append(intent['tag'])
+vector = TfidfVectorizer()
+patterns_scaled=vector.fit_transform(patterns)
+Bot=LogisticRegression(max_iter=100000)
+Bot.fit(patterns_scaled,tags)
+def ChatBot(input_message):
+    input_message=vector.transform([input_message])
+    pred_tag=Bot.predict(input_message)[0]
+    for intent in intents['intents']:
+        if intent['tag']==pred_tag:
+            response=random.choice(intent['responses'])
+            return response
+
+st.title("university AI chatbot")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    response = f"AI ChatBot:" + ChatBot(prompt)
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
+        
+
